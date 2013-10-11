c946e351f9a27701fcb889d82b91c9855d9eca08
Robert Camp <robert.camp@outlook.com>
2013-10-11 17:44:35 -0600

    for weekend

--- adventure/js/myScript.js
+++ adventure/js/myScript.js
@@ -20,13 +20,21 @@ var d12 = function d12 (){
 }
 //end dice
 
+//@param name       string
 var Player = function(name){
 	this.name = name
-	this.health = 4
-	this.tech = 3
+	this.health = 30
+	this.tech = 20
+	this.level = 1
 }
 
-
+Player.prototype.cast = function() {
+	this.tech = (this.tech - 10)
+	$('.playerTech').children().remove();
+	for (var i=0; i<(this.tech); i+=10) {
+		$('.playerTech').append(Creatable.create(['.techHex']))
+	}
+}
 
 //@param name          string
 //@param description   string
@@ -37,21 +45,125 @@ var Monster = function(name, description, health) {
 	this.health = health;
 };
 
-var goblin = new Monster('Goblin', 'test description', 100)
+//@param arr        array
+var Battle = function(arr) {
+	var fight = randomMonster(arr)
+	$('.monsterArea').prepend(Creatable.create(['.monsterName', fight.name]))
 
+		for (var i=0; i<(fight.health); i+=10) {
+		$('.monsterHealth').prepend(Creatable.create(['.healthHex']))
+	}
+}
 
-// $('.monsterArea').append(Creatable.create(['.monster.{name}'.supplant(goblin), '{name} \n {description} \n {health}'.supplant(goblin)]))
+var goblin = new Monster('Goblin', 'test description', 10)
+var kobold = new Monster('Kobold', 'test description', 18)
+var ogre = new Monster('Ogre', 'test description', 75)
+var lizardMan = new Monster('Lizard Man', 'test description', 27)
+var displacerBeast = new Monster('Displacer Beast', 'test description', 48)
+var necromancer = new Monster('Necromancer', 'test description', 112)
+var necroSkele = new Monster('Skeleton Minion', 'test description', 30)
 
+var monsterLv1Array = [goblin, kobold]
+var monsterLv2Array = [goblin, kobold, lizardMan]
+var monsterLv3Array = [kobold, lizardMan]
+var monsterLv4Array = [lizardMan, displacerBeast]
+var bossArray = [ogre, necromancer]
 
-var newPlayer = new Player(prompt('what is your name?'))
-	$('.playerArea').append(Creatable.create(['.playerName', newPlayer.name]))
+var randomMonster = function(arr) {
+	var rand = Math.floor((Math.random()*(arr.length)))
+	return arr[rand]
+}
 
-for (var i=0; i<(newPlayer.tech); i++) {
+//creating a character
+var newPlayer;
+
+$(document).on('click', '.newPlayerButton', function() {
+	newPlayer = new Player(prompt('what is your name?'))
+	$('.playerArea').prepend(Creatable.create([[
+		['.playerName', newPlayer.name + ' the Wimpy'],
+		['.playerLevel',('Level: ' + newPlayer.level)]
+		]]))
+
+	for (var i=0; i<(newPlayer.tech); i+=10) {
 	$('.playerTech').append(Creatable.create(['.techHex']))
 }
-for (var i=0; i<(newPlayer.health); i++) {
+	for (var i=0; i<(newPlayer.health); i+=10) {
 	$('.playerHealth').append(Creatable.create(['.healthHex']))
 }
+	$('.newPlayerButton').fadeOut()
+
+	firstBattle = new Battle(monsterLv1Array)
+});
+
+
+
+var monsterTurn = function() {
+				$('.monsterTurn').fadeIn(800, function() {
+				$('.monsterTurn').fadeOut();
+			})
+		}
+//spell cast
+$(document).on('click', '.spellButton', function() {
+		if(newPlayer.tech >= 10){
+		newPlayer.cast()
+		monsterTurn()
+
+	}
+		else{
+			alert("You don't have enough tech to cast that!")
+			monsterTurn()
+		}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//initial battle
+
+console.log(randomMonster(monsterLv1Array))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
